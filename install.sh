255ded720fddfa1b75a45441bc332e6f9d5a678f
Nisankh Acharjya <36255122+acharjyanisankh@users.noreply.github.com>
2018-10-10 21:06:23 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get install curl wget zsh xsel rlwrap
 curl https://cht.sh/:cht.sh > cheats
-sudo cp cheats /bin/cheats
+sudo mv cheats /bin/cheats
 sudo chmod +x /bin/cheats
 clear
 cheats --help
